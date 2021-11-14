6be9285d5edf03dc2bda131b4a6d3fc15ba984bc
Francesc Travesa <francesc.centrich@navigaglobal.com>
2021-11-14 12:28:33 +0100

    Styles menu

--- src/components/menu/RouterMenu.tsx
+++ src/components/menu/RouterMenu.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const MenuDiv = styled.div`
+`
+
+
+interface menuItemProps {
+    isActive:boolean,
+}
+// note this is a fucking arrow function.
+const navLinkStyles = (isActive:menuItemProps) => ({
+    'display': 'block',
+    'padding': '15px',
+    'background': isActive.isActive ?
+        'url(./assets/background8.jpg)':
+        '#FFF',
+    'color': 'black',
+    'textDecoration': 'none',
+    '&:hover': {
+        'background': '#3CA2F3'
+    }
+})
+
+
 
 export default function RouterMenu() {
+    let navLink =  <NavLink  to={"/about"} />
+    console.log(navLink.type);
+    let stylesForNavLink =  navLinkStyles(navLink.props);
     return (
-        <div>
-            <Link to={"/about"}>About Me</Link>
-            <Link to={"/working-experience"}>Working Experience</Link>
-        </div>
+        <MenuDiv>
+            <NavLink
+                to={"/about"}
+                style={navLinkStyles}
+            >
+
+                About Me
+            </NavLink>
+            <NavLink
+                to={"/working-experience"}
+                style={navLinkStyles}
+            >
+                Working Experience
+            </NavLink>
+        </MenuDiv>
     );
 }
